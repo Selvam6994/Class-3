7798a1eb5d9a6f129e8489064ffebf15f1d723b9
Selvam <107210789+Selvam6994@users.noreply.github.com>
2022-06-21 09:51:40 +0530

    Add files via upload

--- JSON_My_Resume.js
+++ JSON_My_Resume.js
@@ -1,32 +1,32 @@
 let my_Resume = [{
-    Name : "My Name",
+    Name : "S.Selvakumar",
        Location : {
-       "Door number" : "Door number",
-       "Village" :"village",
-       "Town" :  "Town",
-       "PIN CODE" :  "PIN CODE"
+       "Door number" : "2/217 KUDAM NAGAR MAIN ROAD",
+       "Village" :"THANDIKIDI",
+       "Town" : "KODAIKANAL.",
+       "PIN CODE" : 624216
      },
-     Phone : "Phone" ,
-     Email: "selxxxxxxr@gmail.com"
+     Phone : +91_8111033297,
+     Email: "selvamyoor@gmail.com"
      },
        carrier_objective = ("To be a part of an organization where continuous learning and growth is assured and fulfilling the expectations out of my knowledge, skills, experience and there by reach the organization goal."),
       experience_summary = {
-      "Company Details": "xyzs Ltd,Coimbatore.",  
+      "Company Details": "Lakshmi Life Sciences Ltd,Coimbatore.",  
     "Role": "Graduate Engineer Trainee" , 
-    "Nature of work": "Nature of work" ,
+    "Nature of work": "Panel wiring, Pneumatics and sensor selection" ,
     "Duration" : "May 2018- Dec 2018" 
 },
 educational_qualification = {
     "Course" : "B.E./Mechatronics",
-    "Institution" : "ABC Institute of Technology",
+    "Institution" : "Chennai Institute of Technology",
     "CGPA"    : 6.7,
     "Starting year" : 2013,
     "completed year": 2017
 
 },
 Personal_details={
-    "Father's name" : "Father's name",
-    "Mother's name" : "Mother's name",
+    "Father's name" : "Mr.Shanmugasundaram.S",
+    "Mother's name" : "Mrs.Kowsalya.S",
     "Marrital Status": "Single",
     "Nationality"  : "Indian",
     "Languages known": "Tamil,English"
